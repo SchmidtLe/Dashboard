be3045895e5ebfe5b30e349bde1b63229927c26d
SchmidtLe <schmidtle91471@th-nuernberg.de>
2026-02-02 16:55:00 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,12 +4,16 @@ import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  // DAS ist neu – wichtig für GitHub Pages
+  base: '/figma-dashboard/',
+
   plugins: [
     // The React and Tailwind plugins are both required for Make, even if
     // Tailwind is not being actively used – do not remove them
     react(),
     tailwindcss(),
   ],
+
   resolve: {
     alias: {
       // Alias @ to the src directory
